1a2b913ad535b280976463154f93361a9e423980
givolt <givoltoline@hotmail.com>
2024-09-27 22:19:13 +0100

    init: setup

--- setup.py
+++ setup.py
@@ -7,13 +7,13 @@ setup(  name="text_data_visualization",              # Package name
         description="This program offers 10 different types of data visualizations, specifically designed to work with textual data.",
         long_description=open('README.md').read(),
         long_description_content_type="text/markdown",
-        url="https://github.com/yourusername/yourproject",
+        url="https://github.com/GikaVolt/Text_Data_Visualization",
         packages=find_packages(),              # Automatically find packages in the project
         classifiers=[                          # Optional: Classify your package
             "Programming Language :: Python :: 3",
             "License :: OSI Approved :: MIT License",
             "Operating System :: OS Independent"],
-        python_requires='>=3.6',               # Minimum Python version
-        install_requires=[                     # Dependencies
-            "numpy"     ]                      # Example dependency
+        python_requires='>=3.12.2',               # Minimum Python version
+        install_requires=[                        # Dependencies
+            "pandas", "bokeh", "math", "matplotlib.pyplot", "missingno", "plotly.express", "seaborn", "altair", "wordcloud", "numpy", "PIL"]                      # Example dependency
     )
